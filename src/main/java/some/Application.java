8eead73b9307ab4f57493ea5365b5b9390ea72b8
Christoph Baier <baier@atb-bremen.de>
2022-01-03 09:57:27 +0100

    Added erroneous prime generator.

--- src/main/java/some/Application.java
+++ src/main/java/some/Application.java
@@ -3,7 +3,26 @@ package some;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        printPrimesBetweenZeroAndTwenty();
+    }
+
+    private static void printPrimesBetweenZeroAndTwenty() {
+        for (int i = 0; i < 20; i++){
+            if (isPrime(i))
+                System.out.println("Prime number: " + i);
+        }
+    }
+
+    private static boolean isPrime(int potentialPrimeNumber) {
+        if (potentialPrimeNumber <= 0)
+            return false;
+
+        for (int i = 2; i <= potentialPrimeNumber; i++) {
+            if (potentialPrimeNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
